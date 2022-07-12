1a3770483d9cded97a383a1422acc1d929a21163
Stephen R <stephen.r@2hatslogic.com>
2022-07-12 17:16:41 +0530

    some review fixes from last review meeting

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -8,3 +8,13 @@ link.click(function (e) {
     e.preventDefault();
     list.slideToggle(200);
 });
+
+/**
+ * Custom dropdown
+ * 
+ */
+$(".openfrnds").click(function () {
+    $(".right-nav").toggleClass("aa");
+});
+
+
